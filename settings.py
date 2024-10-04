7ad4862c00b20fa7ab227204634265a3bc3e7a6f
Aaron <christiangarduno292@gmail.com>
2024-10-03 22:36:12 -0600

    Actualización de settings.py en el apartado STATICFILES_DIRS

--- settings.py
+++ settings.py
@@ -116,6 +116,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # Aquí se especifica el directorio donde estarán tus archivos estáticos
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
