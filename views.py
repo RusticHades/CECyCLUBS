af01a2bef0d0c90c85733f90cfd0dc4e7b7ced14
Aaron <christiangarduno292@gmail.com>
2024-11-24 17:00:35 -0600

    Se agregan cambios en el views para mostrar el usuario

--- views.py
+++ views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-
-# Create your views here.
 # inicio/views.py
 
 from django.http import HttpResponse
 
 def inicio(request):
-    return render(request,'inicio.html')
+    return render(request, 'inicio.html', {
+        'usuario': request.user,
+    })
